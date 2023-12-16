0395276e39826578bc81dbe3d19d640dfd980e94
Josh <113625417+ProjectAdonis@users.noreply.github.com>
2023-12-16 20:27:39 +0100

    Responsiveness update

--- script.js
+++ script.js
@@ -1,77 +1,78 @@
-$(document).ready( function() {
-  $('.page').scroll( function (){
+$(document).ready(function () {
+  $(".page").scroll(function () {
     if ($(window).width() < 378) {
-      $('.nav').css("background-color","rgba(243,191,2,0.98)");
-      $('.nav').css("border-bottom","1px solid #f3bf02");
+      $(".nav").css("background-color", "rgba(243,191,2,0.98)");
+      $(".nav").css("border-bottom", "1px solid #f3bf02");
       return;
     }
     if ($(this).scrollTop() > 70) {
-      $('.nav').css("background-color","rgba(243,191,2,0.98)");
-      $('.nav').css("border-bottom","1px solid #f3bf02");
+      $(".nav").css("background-color", "rgba(243,191,2,0.98)");
+      $(".nav").css("border-bottom", "1px solid #f3bf02");
     } else {
-      $('.nav').css("background-color","transparent");
-      $('.nav').css("border-bottom","none");
+      $(".nav").css("background-color", "transparent");
+      $(".nav").css("border-bottom", "none");
     }
     if ($(this).scrollTop() > 340) {
-      $('.button-nav-cta').css("display","block");
+      $(".button-nav-cta").css("display", "block");
       $(".button-nav-cta").removeClass("anim_fadeInLeft");
       $(".button-nav-cta").addClass("anim_fadeInRight");
     } else {
-      $('.button-nav-cta').css("display","none");
+      $(".button-nav-cta").css("display", "none");
       $(".button-nav-cta").removeClass("anim_fadeInLeft");
       $(".button-nav-cta").addClass("anim_fadeInRight");
     }
   });
-  
-  $('#scrollup').on('click', function () {
-    $('.page').animate({ scrollTop: 0 }, 800);
+
+  $("#scrollup").on("click", function () {
+    $(".page").animate({ scrollTop: 0 }, 800);
   });
-  
-  $('.page').animate({ scrollTop: 2200 }, 0).animate({ scrollTop: 0 }, 4000);
+
+  $(".page").animate({ scrollTop: 2200 }, 0).animate({ scrollTop: 0 }, 4000);
 });
 
-function jump(h){
-  var container = $('.page'), scrollTo = $('#'+h);
-  container.animate({ scrollTop: scrollTo.offset().top - container.offset().top + container.scrollTop() - 70 }, 800);
+function jump(h) {
+  var container = $(".page"),
+    scrollTo = $("#" + h);
+  container.animate(
+    {
+      scrollTop:
+        scrollTo.offset().top -
+        container.offset().top +
+        container.scrollTop() -
+        70,
+    },
+    800
+  );
 }
 
-
-
-
-var $bg = $('body');
+var $bg = $("body");
 const checkbox = document.getElementById("checkbox");
 
 function changeBackgroundColor() {
   const screenWidth = window.innerWidth;
-  const isDarkMode = localStorage.getItem('theme') === 'dark';
+  const isDarkMode = localStorage.getItem("theme") === "dark";
 
   if (screenWidth < 1350) {
-    $bg.css({ 'background': '#F2BE22' });
+    $bg.css({ background: "#F2BE22" });
   } else {
     if (isDarkMode || checkbox.checked) {
-      $bg.css({ 'background': '#41424C' });
+      $bg.css({ background: "#41424C" });
     } else {
-      $bg.css({ 'background': '#FFFBE9' });
+      $bg.css({ background: "#FFFBE9" });
     }
   }
 }
 
-
-
-
-
-
-
 // Function to save the user's theme choice to localStorage
 function saveThemeChoice() {
-  localStorage.setItem('theme', checkbox.checked ? 'dark' : 'light');
+  localStorage.setItem("theme", checkbox.checked ? "dark" : "light");
   changeBackgroundColor(); // Update the background color immediately when the theme is saved
 }
 
 // Function to load the user's theme choice from localStorage
 function loadSavedTheme() {
-  const savedTheme = localStorage.getItem('theme');
-  if (savedTheme === 'dark') {
+  const savedTheme = localStorage.getItem("theme");
+  if (savedTheme === "dark") {
     checkbox.checked = true;
   } else {
     checkbox.checked = false;
@@ -82,13 +83,12 @@ function loadSavedTheme() {
 loadSavedTheme();
 
 // Event listener for the checkbox
-checkbox.addEventListener("change", function() {
+checkbox.addEventListener("change", function () {
   saveThemeChoice();
 });
 
 // Event listener for window resize
 window.addEventListener("resize", changeBackgroundColor);
-
 
 changeBackgroundColor();
 
